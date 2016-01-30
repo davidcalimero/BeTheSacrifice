376fe3738dad3b85b67c753bc00b2ad3c3900c02
Élvio- <ak4iwa@gmail.com>
2016-01-30 22:19:23 +0000

    inventory uses getIcon (cleaned up debug code)

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -12,12 +12,8 @@ class Inventory : MonoBehaviour
 
 	public IItem[] inventoryPlayer = new IItem[MAX_ITEMS];
 
-	public Texture2D dummyTexture;
-	private Item dummyItem = new Item ();
-
-	public IItem useItem(int inventorySlot)
+	public IItem useItem(uint inventorySlot)
 	{
-		Debug.Log ("ime here");
 		if (inventorySlot < MAX_ITEMS && inventorySlot >= 0 ){
 			IItem ret = inventoryPlayer [inventorySlot];
 			inventoryPlayer [inventorySlot] = null;
@@ -43,10 +39,10 @@ class Inventory : MonoBehaviour
 			GUI.DrawTexture(new Rect(iconSpace*4f + xOffset*3f, Screen.height - 2f*iconSize,iconSize , iconSize), itemSlotBorder);
 
 			//draw the icons on the squares
-			if (inventoryPlayer [0] != null) GUI.DrawTexture(new Rect(iconSpace*1f + xOffset*0f, Screen.height - 2f*iconSize, iconSize, iconSize), dummyTexture);
-			if (inventoryPlayer [1] != null) GUI.DrawTexture(new Rect(iconSpace*2f + xOffset*1f, Screen.height - 2f*iconSize, iconSize, iconSize), dummyTexture);
-			if (inventoryPlayer [2] != null) GUI.DrawTexture(new Rect(iconSpace*3f + xOffset*2f, Screen.height - 2f*iconSize,iconSize , iconSize), dummyTexture);
-			if (inventoryPlayer [3] != null) GUI.DrawTexture(new Rect(iconSpace*4f + xOffset*3f, Screen.height - 2f*iconSize,iconSize , iconSize), dummyTexture);
+			if (inventoryPlayer [0] != null) GUI.DrawTexture(new Rect(iconSpace*1f + xOffset*0f, Screen.height - 2f*iconSize, iconSize, iconSize), inventoryPlayer[0].getIcon());
+			if (inventoryPlayer [1] != null) GUI.DrawTexture(new Rect(iconSpace*2f + xOffset*1f, Screen.height - 2f*iconSize, iconSize, iconSize), inventoryPlayer[1].getIcon());
+			if (inventoryPlayer [2] != null) GUI.DrawTexture(new Rect(iconSpace*3f + xOffset*2f, Screen.height - 2f*iconSize,iconSize , iconSize), inventoryPlayer[2].getIcon());
+			if (inventoryPlayer [3] != null) GUI.DrawTexture(new Rect(iconSpace*4f + xOffset*3f, Screen.height - 2f*iconSize,iconSize , iconSize), inventoryPlayer[3].getIcon());
 		}
 
 		else if(this.gameObject.name.Equals("Player2")){
@@ -55,37 +51,10 @@ class Inventory : MonoBehaviour
 			GUI.DrawTexture(new Rect(Screen.width - iconSpace*2f - xOffset*2f - 2f*iconSpace, Screen.height - 2f*iconSize, iconSize, iconSize), itemSlotBorder);
 			GUI.DrawTexture(new Rect(Screen.width - iconSpace*3f - xOffset*3f - 2f*iconSpace, Screen.height - 2f*iconSize, iconSize, iconSize), itemSlotBorder);
 
-			if (inventoryPlayer [0] != null) GUI.DrawTexture(new Rect(Screen.width - iconSpace*0f - xOffset*0f - 2f*iconSpace, Screen.height - 2f*iconSize, iconSize, iconSize), dummyTexture);
-			if (inventoryPlayer [1] != null) GUI.DrawTexture(new Rect(Screen.width - iconSpace*1f - xOffset*1f - 2f*iconSpace, Screen.height - 2f*iconSize, iconSize, iconSize), dummyTexture);
-			if (inventoryPlayer [2] != null) GUI.DrawTexture(new Rect(Screen.width - iconSpace*2f - xOffset*2f - 2f*iconSpace, Screen.height - 2f*iconSize, iconSize, iconSize), dummyTexture);
-			if (inventoryPlayer [3] != null) GUI.DrawTexture(new Rect(Screen.width - iconSpace*3f - xOffset*3f - 2f*iconSpace, Screen.height - 2f*iconSize, iconSize, iconSize), dummyTexture);
+			if (inventoryPlayer [0] != null) GUI.DrawTexture(new Rect(Screen.width - iconSpace*0f - xOffset*0f - 2f*iconSpace, Screen.height - 2f*iconSize, iconSize, iconSize), inventoryPlayer[0].getIcon());
+			if (inventoryPlayer [1] != null) GUI.DrawTexture(new Rect(Screen.width - iconSpace*1f - xOffset*1f - 2f*iconSpace, Screen.height - 2f*iconSize, iconSize, iconSize), inventoryPlayer[1].getIcon());
+			if (inventoryPlayer [2] != null) GUI.DrawTexture(new Rect(Screen.width - iconSpace*2f - xOffset*2f - 2f*iconSpace, Screen.height - 2f*iconSize, iconSize, iconSize), inventoryPlayer[2].getIcon());
+			if (inventoryPlayer [3] != null) GUI.DrawTexture(new Rect(Screen.width - iconSpace*3f - xOffset*3f - 2f*iconSpace, Screen.height - 2f*iconSize, iconSize, iconSize), inventoryPlayer[3].getIcon());
 		}
 	}
-
-	void Update(){
-		if (this.gameObject.name.Equals ("Player1")) {
-			if (Input.GetKeyDown ("z")) {
-				Debug.Log ("z");
-				useItem (0);
-			}
-			if (Input.GetKeyDown ("x")) {
-				Debug.Log ("x");
-				useItem (1);
-			}
-			if (Input.GetKeyDown ("c")) {
-				Debug.Log ("c");
-				useItem (2);
-			}
-			if (Input.GetKeyDown ("v")) {
-				Debug.Log ("v");
-				useItem (3);
-			}
-
-			if (Input.GetKeyDown ("m")) {
-				Debug.Log ("m");
-				addItem (dummyItem);
-			}
-		}
-	}
-
 }
