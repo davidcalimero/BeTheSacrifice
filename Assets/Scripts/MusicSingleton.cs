2f7024014d4b4beaf3cc268fb40f46dc4b5f2353
Élvio- <ak4iwa@gmail.com>
2016-01-30 16:22:04 +0000

    Music stuff

---
+++ Assets/Scripts/MusicSingleton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicSingleton : MonoBehaviour {
+
+	private static MusicSingleton instance;
+
+	public static MusicSingleton GetInstance() {
+		return instance; 
+	}
+	
+	void Awake() { 
+		if (instance != null && instance != this) 
+			{ Destroy(this.gameObject); return; 
+		} else { 
+			instance = this; 
+		} 
+		DontDestroyOnLoad(this.gameObject); 
+	}
+}
+
