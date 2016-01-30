94d80e1416f499ddaa185ad711b21f1dff46eb1c
davidcalimero <david.d.silva@ist.utl.pt>
2016-01-30 08:40:16 +0000

    base code

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
+
+class Character : MonoBehaviour
+{
+    public float runSpeed = 5;
+
+    //Character state
+    private bool facingRight = true;
+    private Vector2 direction;
+
+    private Rigidbody body;
+    private Animator animator;
+    
+    void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        //Update animations
+        animator.SetFloat("Speed", direction.magnitude);
+
+        if ((body.velocity.x > 0 && !facingRight) || (body.velocity.x < 0 && facingRight))
+        {
+            Flip();
+        }
+
+        body.velocity = new Vector3(direction.x, 0, direction.y) * runSpeed;
+    }
+
+    public void Move(Vector2 direction)
+    {
+        this.direction = direction;
+    }
+
+    protected void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Character))]
+
+class PlayerController : MonoBehaviour
+{
+    private Character character;
+
+    void Awake()
+    {
+        character = GetComponent<Character>(); 
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Update()
+    {
+
+    }
+
+    protected void Move()
+    {
+        Vector2 direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        character.Move(direction);
+    }
+}
