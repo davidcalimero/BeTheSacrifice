91f45d650354f2f941ba9ccee8cfd45c970be235
Élvio- <ak4iwa@gmail.com>
2016-01-30 18:56:50 +0000

    player ui and inventory ui

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -3,9 +3,31 @@
 class Inventory : MonoBehaviour
 {
     public GameObject item;
+	public Texture2D itemSlot;
+
+	private float iconSpace = 30.0f;
+	private float xOffset = 15.0f;
+	private float iconSize = 30.0f;
+
 
     public IItem RequestItem(string inputKey)
     {
         return item.GetComponent<IItem>();
     }
+
+	void OnGUI(){
+		if (this.gameObject.name.Equals ("Player1")) {
+			GUI.DrawTexture(new Rect(iconSpace, Screen.height - 2f*iconSize, iconSize, iconSize), itemSlot);
+			GUI.DrawTexture(new Rect(iconSpace*2f + xOffset, Screen.height - 2f*iconSize, iconSize, iconSize), itemSlot);
+			GUI.DrawTexture(new Rect(iconSpace*3f + xOffset*2f, Screen.height - 2f*iconSize,iconSize , iconSize), itemSlot);
+			GUI.DrawTexture(new Rect(iconSpace*4f + xOffset*3f, Screen.height - 2f*iconSize,iconSize , iconSize), itemSlot);
+		}
+
+		else if(this.gameObject.name.Equals("Player2")){
+			GUI.DrawTexture(new Rect(Screen.width - iconSpace*0f - xOffset*0f - 2f*iconSpace, Screen.height - 2f*iconSize, iconSize, iconSize), itemSlot);
+			GUI.DrawTexture(new Rect(Screen.width - iconSpace*1f - xOffset*1f - 2f*iconSpace, Screen.height - 2f*iconSize, iconSize, iconSize), itemSlot);
+			GUI.DrawTexture(new Rect(Screen.width - iconSpace*2f - xOffset*2f - 2f*iconSpace, Screen.height - 2f*iconSize, iconSize, iconSize), itemSlot);
+			GUI.DrawTexture(new Rect(Screen.width - iconSpace*3f - xOffset*3f - 2f*iconSpace, Screen.height - 2f*iconSize, iconSize, iconSize), itemSlot);
+		}
+	}
 }
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+	//Player _player;
+
+	public Texture2D lifeBar;
+	public float lifebarSize;
+	private float barWidth = 30.0f;
+	private float widthFix = 5.0f;
+
+	void Start(){
+		//_playerCharacter = this.gameObject.GetComponent <Player> ();
+
+	}
+
+	void OnGUI(){
+		if (this.gameObject.name.Equals ("Player1")) {
+			//_lifebarSize = _player.lifeAmmount;
+
+			//GUI.Label(new Rect (0, 50, 100, 30), _player.lifeAmmount.ToString());
+			GUI.DrawTexture(new Rect(Screen.width*0.5f - 15f, 50, -(lifebarSize), barWidth), lifeBar);
+
+		}
+		else if(this.gameObject.name.Equals("Player2")){
+			//_lifebar_size = playerManager.lifeAmmount;
+
+			//GUI.Label(new Rect (xOffset + 0, yOffset + 50, 100, 30), _player.lifeAmmount.ToString());
+			GUI.DrawTexture(new Rect(Screen.width*0.5f + 15, 50, lifebarSize, barWidth), lifeBar);
+		}
+	}
+}
