b327803847d0af31487f263c5ebcc640591b227d
davidcalimero <david.d.silva@ist.utl.pt>
2016-01-31 15:40:35 +0000

    crate damage

--- Assets/Scripts/Crate.cs
+++ Assets/Scripts/Crate.cs
@@ -7,8 +7,17 @@ class Crate : MonoBehaviour{
     // Which prefab drops down
     // PELO AMOR DA SANTA NAO METER NADA!!!!!
     public GameObject Item;
+    public int fallDamage = 10;
 
     void OnTriggerEnter(Collider col){
+
+        if (col.gameObject.tag == "Player")
+        {
+            IPlayer player = col.gameObject.GetComponent<IPlayer>();
+            player.ChangeLife(-fallDamage);
+            player.Push(Vector3.zero);
+        }
+
         Vector3 position = transform.position;
         position.y += 0.5f;
         Instantiate(Item, position, new Quaternion());
